1144d02cfc0e7204f7de901be2cac8b6608522c6
Ballas <jeballas@kmc.local>
2018-06-05 11:19:37 -0400

    fix ie issues

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  public year: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.year = new Date().getFullYear();
   }
 
 }
