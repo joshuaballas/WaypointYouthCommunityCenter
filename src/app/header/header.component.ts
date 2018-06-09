4391f3aad20e8bfa27d0b3bc5a69cf14d131effc
rockguitarist1 <wzsoccerplyr@aim.com>
2018-06-09 13:45:27 -0400

    Close hamburger menu on nav change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,10 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   public isCollapsed = true;
 
-  constructor() { }
+  constructor(private router: Router) {
+    router.events.subscribe(() => {
+      if (!this.isCollapsed) { this.isCollapsed = true; }
+    });
+  }
 
 }
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
-export class TeamComponent implements OnInit {
+export class TeamComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
