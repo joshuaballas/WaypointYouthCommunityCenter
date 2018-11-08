a4aa84d5a8e8031021e3a032ebb22189b22f3fe2
rockguitarist1 <wzsoccerplyr@aim.com>
2018-11-07 21:00:26 -0500

    Add in drone header video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public mobile: boolean;
 
   constructor() { }
+
+  ngOnInit() {
+    if (window.screen.width >= 600) { this.mobile = true; }
+  }
 
 }
